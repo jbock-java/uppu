69383766f328ab1f487479ea0e96c388409b305b
jbock <jbock-java@gmx.de>
2022-11-26 21:55:31 +0100

    how it feels to chew 12 gum

--- src/test/java/uppu/view/PermutationViewChecker.java
+++ src/test/java/uppu/view/PermutationViewChecker.java
@@ -5,7 +5,10 @@ import uppu.engine.Animation;
 import uppu.model.BiCommand;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static uppu.model.BiCommand.command;
 
@@ -22,10 +25,15 @@ class PermutationViewChecker {
         List<Permutation> permutations = Permutation.symmetricGroup(5).stream()
                 .filter(p -> p.order() == 5)
                 .toList();
+        Map<Set<Permutation>, Permutation> m = new LinkedHashMap<>();
         List<BiCommand> commands = new ArrayList<>();
         commands.add(BiCommand.showState());
         commands.add(BiCommand.wait(20));
         for (Permutation p : permutations) {
+            m.putIfAbsent(Set.of(p, p.invert()), p);
+        }
+        List<Permutation> values = List.copyOf(m.values());
+        for (Permutation p : values) {
             commands.addAll(commands(p));
         }
         Animation.create(view).startAnimation(commands);
